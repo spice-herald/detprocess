2e008cdb2291bc38758d95f4b4f70a7885d9a4b6
Samuel Watkins <samwatkins@berkeley.edu>
2021-06-25 17:34:25 -0700

    update script to include needed paths

--- scripts/process.py
+++ scripts/process.py
@@ -10,11 +10,22 @@ from detprocess.process import process_data
 
 dview.execute('from detprocess.process import process_data')
 
-mydict = {'process_data': process_data}
+path_to_yaml = '/full/path/to/yaml/process.yaml'
+savepath = '/full/path/to/save/processed/data/'
+
+mydict = {
+    'process_data': process_data,
+    'path_to_yaml': path_to_yaml,
+    'savepath': savepath,
+}
+
 dview.push(mydict)
 
 from glob import glob
 files = glob('/full/path/to/folder/to/process/*')
+
+def wrapper(f):
+    return process_data(f, path_to_yaml, savepath=savepath)
 
 import time
 time.time()
