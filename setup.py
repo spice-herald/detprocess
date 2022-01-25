a0ea2cdbdffc6bb2f9f069829c2901215174d747
Samuel Watkins <35979561+slwatkins@users.noreply.github.com>
2022-01-24 16:28:10 -0800

    update to v0.1.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class CleanCommand(Command):
 
 setup(
     name="detprocess",
-    version="0.0.2",
+    version="0.1.0",
     description="Detector Data Processing Package",
     long_description=long_description,
     long_description_content_type='text/markdown',
