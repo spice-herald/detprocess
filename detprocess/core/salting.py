ed68e30a9e7946daea79764a2e71f091adbd7c6c
Michael Williams <michrw@umich.edu>
2024-08-13 10:53:09 -0700

    basic dual channel salting, no processing code

---
+++ detprocess/core/salting.py
@@ -0,0 +1,37 @@
+import numpy as np
+import os
+import vaex as vx
+import pandas as pd
+import qetpy as qp
+from math import log10, floor
+import math
+import array
+from scipy.signal import correlate
+from scipy.fft import ifft, fft, next_fast_len
+
+class Salting:
+    """
+    Class for injecting salt into datasets for multiple channels. Can be used to
+    understand cut efficiencies.
+
+    Attributes
+    ----------
+    asd:asdfasdfasdfasdfasdfasdf
+
+    """
+
+    def __init__(self, verbose=True):
+        """
+        Initialize class
+
+        Parameters:
+        ----------
+
+        verbose : bool, optional
+          display information
+
+
+        """
+        
+    def autocorrelate():
+      asdf
