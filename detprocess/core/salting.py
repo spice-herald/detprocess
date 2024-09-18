291388a18246398eae1fb259e8b7b72a1b27bff2
Michael Williams <michaelwilliams@lbl.gov>
2024-09-18 15:19:40 -0700

    up to date 09/18/24

--- detprocess/core/salting.py
+++ detprocess/core/salting.py
@@ -4,12 +4,22 @@ import vaex as vx
 import pandas as pd
 import qetpy as qp
 from math import log10, floor
+from glob import glob
+from pathlib import Path
+import pytesdaq.io as h5io
 import math
 import array
+from detprocess.process.randoms import Randoms
+from detprocess.core.filterdata import FilterData
 from scipy.signal import correlate
 from scipy.fft import ifft, fft, next_fast_len
 
-class Salting:
+__all__ = [
+    'Salting'
+]
+
+
+class Salting(FilterData):
     """
     Class for injecting salt into datasets for multiple channels. Can be used to
     understand cut efficiencies.
@@ -20,7 +30,7 @@ class Salting:
 
     """
 
-    def __init__(self, verbose=True):
+    def __init__(self, verbose=True,yaml_path=None):
         """
         Initialize class
 
@@ -32,12 +42,537 @@ class Salting:
 
 
         """
+
+        # initialize raw data dictionary
+        self._raw_data = None
+        self._group_name = None
+        self._raw_base_path = None
+        self._series_list = None
+        self._detector_config = None
+        self._ivdidv_data = dict()
+
         
-    def autocorrelate():
-      asdf
-    def inject_salt():
-      asdfa
+        # intialize randoms dataframe
+        self._dataframe = None
+
+        # intialize event list
+        self._event_list = None
+
+        # sample rate stored for convenience
+        self._fs = None
+    
+        # offset
+        self._offset = dict()
+
+        self._verbose = verbose
+
+        if yaml_path is not None:
+            if os.path.isfile(yaml_path):
+                self._save_path = os.path.dirname(yaml_path)
+                self._file_name = os.path.basename(yaml_path)
+            elif os.path.isdir(yaml_path):
+                self._save_path = yaml_path
+            else:
+                raise ValueError('ERROR: "yaml_path" should be a '
+                                 'file or path!')
+        
+        super().__init__(verbose=verbose)
+
+    def get_detector_config(self, channel):
+        """
+        get detector config
+        """
+        if self._detector_config is None:
+            print('WARNING: No data has been set yet! '
+                  'Returning None ')
+            return None
+        elif channel not in self._detector_config.keys():
+            print(f'WARNING: No channel {channel}  found! '
+                  f'Returning None ')
+            return None
+        return self._detector_config[channel]
+                
+    def get_sample_rate(self):
+        """
+        Get sample rate in Hz ("calc_psd" needs to be 
+        called before)
+        """
+
+        return self._fs
+
+    def clear_randoms(self):
+        """
+        Clear internal data, however
+        keep self._filter_data
+        """
+
+        # clear data
+        self._dataframe = None
+        self._event_list = None
+        self._raw_data = None
+        self._group_name = None
+        self._raw_base_path = None
+        self._series_list = None
+        self._detector_config = None
+        self._fs = None
+        self._offset = dict()
+        
+    def set_randoms(self, raw_path, series=None,
+                    dataframe=None,
+                    event_list=None):
+        """
+        Set raw data path and vaex dataframe 
+        with randoms events (either dataframe directly
+        or path to vaex hdf5 files)
+        """
+        
+        # initialize data
+        self.clear_randoms()
+        
+
+        # check arguments
+        if (dataframe is not None
+            and event_list is not None):
+            raise ValueError('ERROR: choose between "dataframe" and '
+                             '"event_list", not both')
+        
+        # get file list
+        raw_data, output_base_path, group_name = (
+            self._get_file_list(raw_path,
+                                series=series)
+        )
+        
+        if not raw_data:
+            raise ValueError('No raw data files were found! '
+                             + 'Check configuration...')
+        
+        # store as internal data
+        self._raw_base_path = output_base_path
+        self._raw_data = raw_data
+        self._group_name = group_name
+        self._series_list = list(raw_data.keys())
+        self._detector_config = dict()
+
+        # check dataframe
+        if dataframe is not None:
+            
+            if isinstance(dataframe, vx.dataframe.DataFrame):
+                if len(dataframe)<1:
+                    raise ValueError('ERROR: No event found in the datafame!')
+            else:
+                dataframe = self._load_dataframe(dataframe)
+
+            self._dataframe = dataframe
+
+        elif event_list is not None:
+            self._event_list = event_list
+
+        # check filter data
+        if self._filter_data:
+            print('WARNING: Some noise data have been previously saved. '
+                  'Use "describe()" to check. If needed clear data '
+                  'using "clear_data(channels=None, tag=None)" function!')
+            
+            
+    def generate_randoms(self, raw_path, series=None,
+                         random_rate=None,
+                         nevents=None,
+                         min_separation_msec=100,
+                         edge_exclusion_msec=50,
+                         restricted=False,
+                         calib=False,
+                         ncores=1):
+        """
+        Generate randoms from continuous data
+        """
+
+        # initialize data
+        self.clear_randoms()
+
+        # get file list
+        raw_data, output_base_path, group_name = (
+            self._get_file_list(raw_path,
+                                series=series,
+                                restricted=restricted,
+                                calib=calib)
+        )
+        
+        if not raw_data:
+            raise ValueError('No raw data files were found! '
+                             + 'Check configuration...')
+        
+        # store as internal data
+        self._raw_data = raw_data
+        self._group_name = group_name
+        self._raw_base_path = output_base_path
+        self._series_list = list(raw_data.keys())
+        self._detector_config = dict()
+
+             
+        # generate randoms
+        rand_inst = Randoms(raw_path, series=series,
+                            verbose=self._verbose,
+                            restricted=restricted,
+                            calib=calib)
+        
+        self._dataframe = rand_inst.process(
+            random_rate=random_rate,
+            nrandoms=nevents,
+            min_separation_msec=min_separation_msec,
+            edge_exclusion_msec=edge_exclusion_msec,
+            lgc_save=False,
+            lgc_output=True,
+            ncores=ncores
+        )
+
+        # check filter data
+        if self._filter_data:
+            print('WARNING: Some noise data have been previously saved. '
+                  'Use "describe()" to check. If needed clear data '
+                  'using "clear_data(channels=None, tag=None)" function!')
+            
+    def _get_file_list(self, file_path,
+                       series=None,
+                       is_raw=True,
+                       restricted=False,
+                       calib=False):
+        """
+        Get file list from path. Return as a dictionary
+        with key=series and value=list of files
+
+        Parameters
+        ----------
+
+        file_path : str or list of str 
+           raw data group directory OR full path to HDF5  file 
+           (or list of files). Only a single raw data group 
+           allowed 
+        
+        series : str or list of str, optional
+            series to be process, disregard other data from raw_path
+
+        restricted : boolean
+            if True, use restricted data 
+            if False (default), exclude restricted data
+
+        Return
+        -------
+        
+        series_dict : dict 
+          list of files for splitted inot series
+
+        base_path :  str
+           base path of the raw data
+
+        group_name : str
+           group name of raw data
+
+        """
+
+        # convert file_path to list 
+        if isinstance(file_path, str):
+            file_path = [file_path]
+            
+            
+        # initialize
+        file_list = list()
+        base_path = None
+        group_name = None
+
+
+        # loop files 
+        for a_path in file_path:
+                   
+            # case path is a directory
+            if os.path.isdir(a_path):
+
+                if base_path is None:
+                    base_path = str(Path(a_path).parent)
+                    group_name = str(Path(a_path).name)
+                            
+                if series is not None:
+                    if series == 'even' or series == 'odd':
+                        file_name_wildcard = series + '_*.hdf5'
+                        file_list = glob(a_path + '/' + file_name_wildcard)
+                    else:
+                        if not isinstance(series, list):
+                            series = [series]
+                        for it_series in series:
+                            file_name_wildcard = '*' + it_series + '_*.hdf5'
+                            file_list.extend(glob(a_path + '/' + file_name_wildcard))
+                else:
+                    file_list = glob(a_path + '/*.hdf5')
+               
+                # check a single directory
+                if len(file_path) != 1:
+                    raise ValueError('Only single directory allowed! ' +
+                                     'No combination files and directories')
+                
+                    
+            # case file
+            elif os.path.isfile(a_path):
+
+                if base_path is None:
+                    base_path = str(Path(a_path).parents[1])
+                    group_name = str(Path(Path(a_path).parent).name)
+                    
+                if a_path.find('.hdf5') != -1:
+                    if series is not None:
+                        if series == 'even' or series == 'odd':
+                            if a_path.find(series) != -1:
+                                file_list.append(a_path)
+                        else:
+                            if not isinstance(series, list):
+                                series = [series]
+                            for it_series in series:
+                                if a_path.find(it_series) != -1:
+                                    file_list.append(a_path)
+                    else:
+                        file_list.append(a_path)
+
+            else:
+                raise ValueError('File or directory "' + a_path
+                                 + '" does not exist!')
+            
+        if not file_list:
+            raise ValueError('ERROR: No raw input data found. Check arguments!')
+
+        # sort
+        file_list.sort()
+
+      
+        # convert to series dictionary so can be easily split
+        # in multiple cores
+        
+        series_dict = dict()
+        h5reader = h5io.H5Reader()
+        series_name = None
+        file_counter = 0
+        
+        for afile in file_list:
+
+            file_name = str(Path(afile).name)
+                        
+            # skip if filter file
+            if 'filter_' in file_name:
+                continue
+
+            # skip didv
+            if ('didv_' in file_name
+                or 'iv_' in file_name):
+                continue
+                      
+            if 'treshtrig_' in file_name:
+                continue
+
+            # calibration
+            if (calib
+                and 'calib_' not in file_name):
+                continue
+
+            # not calibration
+            if not calib:
+                
+                if 'calib_' in file_name:
+                    continue
+                            
+                # restricted
+                if (restricted
+                    and 'restricted' not in file_name):
+                    continue
+
+                # not restricted
+                if (not restricted
+                    and 'restricted' in file_name):
+                    continue
+                      
+            # append file if series already in dictionary
+            if (series_name is not None
+                and series_name in afile
+                and series_name in series_dict.keys()):
+
+                if afile not in series_dict[series_name]:
+                    series_dict[series_name].append(afile)
+                    file_counter += 1
+                continue
+            
+            # get metadata
+            if is_raw:
+                metadata = h5reader.get_metadata(afile)
+                series_name = h5io.extract_series_name(metadata['series_num'])
+            else:
+                sep_start = file_name.find('_I')
+                sep_end = file_name.find('_F')
+                series_name = file_name[sep_start+1:sep_end]
+                              
+            if series_name not in series_dict.keys():
+                series_dict[series_name] = list()
+
+            # append
+            if afile not in series_dict[series_name]:
+                series_dict[series_name].append(afile)
+                file_counter += 1
+       
+            
+        if self._verbose:
+            msg = ' raw data file(s) from '
+            if not is_raw:
+                msg = ' dataframe file(s) from '
+                
+            print('INFO: Found total of '
+                  + str(file_counter)
+                  + msg
+                  + str(len(series_dict.keys()))
+                  + ' different series number!')
+
+      
+        return series_dict, base_path, group_name
+    
+    def set_iv_didv_results_from_file(self, file_name,
+                                      poles=2,
+                                      channels=None):
+        """
+        set dIdV and/or IV sweep results from an HDF5 file
+        """
+
+        self.load_hdf5(file_name)
+
+        if channels is None:
+            channels = self._filter_data.keys()
+            if not channels:
+                raise ValueError(f'ERROR: No data loaded... '
+                                 f'Check file {file_name}')
+        elif isinstance(channels, str):
+            channels = [channels]
+       
+        for chan in channels:
+
+            # check if filter data
+            if chan not in self._filter_data.keys():
+                raise ValueError(f'ERROR: No data loaded for channel {chan}. '
+                                 f'Check file {file_name} !')
+            # dIdV results
+            didv_results = None
+            try:
+                didv_results = self.get_didv_results(chan, poles=poles)
+            except:
+                print(f'WARNING: No {poles}-poles dIdV results found for '
+                      f'channel {chan}!')
+
+            # IV results
+            ivsweep_results = None
+            try:
+                ivsweep_results = self.get_ivsweep_results(chan)
+            except:
+                pass
+
+
+            self.set_iv_didv_results_from_dict(
+                chan,
+                didv_results=didv_results,
+                poles=poles,
+                ivsweep_results=ivsweep_results
+            )
+   
+    def set_iv_didv_results_from_dict(self, channel,
+                                      didv_results=None,
+                                      poles=2,
+                                      ivsweep_results=None):
+        """
+        Set didv from dictionary for specified channel
+        """
+
+        # check if channel exist
+        if channel not in self._ivdidv_data.keys():
+            self._ivdidv_data[channel] = dict()
+
+
+        # save poles
+        self._poles = poles
+   
+        # dIdV results
+        if didv_results is not None:
+
+            if poles is None:
+                raise ValueError('ERROR: dIdV poles (2 or 3) required!')
+
+            # add to filter data
+            metadata = None
+            if 'metadata' in didv_results:
+                metadata = didv_results['metadata']
+            self.set_didv_results(
+                channel,
+                didv_results,
+                poles=poles,
+                metadata=metadata
+            )
+
+            # Add small signal parameters
+            if 'smallsignalparams' in didv_results.keys():
+                self._ivdidv_data[channel]['smallsignalparams'] = (
+                    didv_results['smallsignalparams'].copy()
+                )
+            else:
+                raise ValueError(f'ERROR: dIdV fit results '
+                                 f'does not contain "smallsignalparams" '
+                                 f'for channel {channel}!')
+            
+            # add "biasparams"
+            if ('biasparams' in didv_results.keys()
+                and didv_results['biasparams'] is not None):
+                self._ivdidv_data[channel]['biasparams'] = didv_results['biasparams']
+
+
+        # IV results
+        if ivsweep_results is not None:
+
+            # add to filter data
+            self.set_ivsweep_results(
+                channel,
+                ivsweep_results,
+                'noise')
+
+            # add to noise data
+            if 'biasparams' not in self._ivdidv_data[channel]:
+                self._ivdidv_data[channel]['biasparams'] = ivsweep_results
+            else:
+                self._ivdidv_data[channel]['biasparams'].update(ivsweep_results)
+
+            # add more quantities
+            if 'rn' not in  self._ivdidv_data[channel]['biasparams']:
+                self._ivdidv_data[channel]['biasparams']['rn'] = (
+                    ivsweep_results['rn']
+                )
+
+            # add more quantities
+            if 'rp' not in  self._ivdidv_data[channel]['biasparams']:
+                self._ivdidv_data[channel]['biasparams']['rp'] = (
+                    ivsweep_results['rp']
+                )
+                
+
+            if 'rshunt' not in self._ivdidv_data[channel]['biasparams']:
+                self._ivdidv_data[channel]['biasparams']['rshunt'] = (
+                    ivsweep_results['rshunt']
+                )
+                
+            # inductance
+            if 'normal_didv_fit_L' in ivsweep_results.keys():
+                self.set_inductance(channel,
+                                    ivsweep_results['normal_didv_fit_L'],
+                                    'normal')
+
+            if 'sc_didv_fit_L' in ivsweep_results.keys():
+                self.set_inductance(channel,
+                                    ivsweep_results['sc_didv_fit_L'],
+                                    'sc')
+                
+
     def guassian_smear():
        asdf
     def dm_distribution():
        asf
+    def autocorrelate():
+      asdf
+    def inject_salt():
+      asdfa
