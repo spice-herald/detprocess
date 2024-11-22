6178dee678dd249778abed0f30f332fdf717b072
Bruno Serfass <serfass@berkeley.edu>
2024-11-21 17:43:37 -0800

    new release

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
         'scipy',
         'matplotlib',
         'pyyaml',
-        'qetpy>=1.7.5',
+        'qetpy>=1.7.6',
         'pandas',
         'pytesdaq>=0.4.1',
         'scikit-image',
