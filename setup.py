9b284b4061b67bc892fedfb2c92af210ea7eca87
Bruno Serfass <serfass@berkeley.edu>
2024-11-27 11:00:36 -0800

    improvement performance

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class CleanCommand(Command):
 
 setup(
     name="detprocess",
-    version="0.5.2",
+    version="0.5.3",
     description="Detector Data Processing Package",
     long_description=long_description,
     long_description_content_type='text/markdown',
